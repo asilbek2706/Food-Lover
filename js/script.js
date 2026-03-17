169eb1642500ddad379763ba24137f788f803eaa
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-17 21:43:44 +0500

    feat(ui): add offers API fallback rendering

--- js/script.js
+++ js/script.js
@@ -184,44 +184,54 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    const fallbackOffers = [
+        {
+            src: './img/offer1.png',
+            alt: 'Quattro Pasta',
+            title: 'Quattro Pasta',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.',
+            discount: 55,
+            sale: 20,
+        },
+        {
+            src: './img/offer2.png',
+            alt: 'Vegertarian Pasta',
+            title: 'Vegertarian Pasta',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.',
+            discount: 65,
+            sale: 22,
+        },
+        {
+            src: './img/offer3.png',
+            alt: 'Gluten-Free Pasta',
+            title: 'Gluten-Free Pasta',
+            descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.',
+            discount: 57,
+            sale: 25,
+        },
+    ];
+
+    function renderOffers(data) {
+        data.forEach((offer) => {
+            const { src, alt, title, descr, discount, sale } = offer;
+            new OfferMenu(src, alt, title, descr, discount, sale, '.offers-items').render();
+        });
+    }
+
     fetch('http://localhost:3000/offers', {
         method: 'GET',
         headers: { 'content-type': 'application/json' },
     })
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((offer) => {
-                const { src, alt, title, descr, discount, sale } = offer;
-                new OfferMenu(src, alt, title, descr, discount, sale, '.offers-items').render();
-            });
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Offers request failed with status ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => renderOffers(data))
+        .catch(() => {
+            renderOffers(fallbackOffers);
         });
-
-    // const offers = [
-    //     {
-    //         src: './img/offer1.png',
-    //         alt: 'Quattro Pasta',
-    //         title: 'Quattro Pasta',
-    //         descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.',
-    //         discount: 55,
-    //         sale: 20,
-    //     },
-    //     {
-    //         src: './img/offer2.png',
-    //         alt: 'Vegertarian Pasta',
-    //         title: 'Vegertarian Pasta',
-    //         descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.',
-    //         discount: 65,
-    //         sale: 22,
-    //     },
-    //     {
-    //         src: './img/offer3.png',
-    //         alt: 'Gluten-Free Pasta',
-    //         title: 'Gluten-Free Pasta',
-    //         descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.',
-    //         discount: 57,
-    //         sale: 25,
-    //     },
-    // ];
 
     class DayTime {
         constructor(src, alt, title, start, end, parentSelector) {
